b8a6841f06595b87d57ed8fb9f5a03ac4e078ca2
Erwin Bonsma <eriban@users.sourceforge.net>
2022-02-27 10:59:48 +0100

    Add speed control and view modes

--- PocketGardenGB.ino
+++ PocketGardenGB.ino
@@ -11,8 +11,13 @@
 UpdateFunction updateFunction;
 DrawFunction drawFunction;
 
+constexpr int max_step_wait = 6;
+constexpr int num_view_modes = 6;
+
 int cx, cy;
 bool paused = true;
+int view_mode = 4;
+int step_wait = 5;
 
 std::array<LifeCa, 4> cas;
 
@@ -25,16 +30,32 @@ void displayCpuLoad() {
 
 void testUpdate() {
   if (gb.buttons.pressed(BUTTON_LEFT)) {
-    cx = (cx + W - 1) % W;
+    if (paused) {
+      cx = (cx + W - 1) % W;
+    } else {
+      view_mode = (view_mode + num_view_modes - 1) % num_view_modes;
+    }
   }
   if (gb.buttons.pressed(BUTTON_RIGHT)) {
-    cx = (cx + 1) % W;
+    if (paused) {
+      cx = (cx + 1) % W;
+    } else {
+      view_mode = (view_mode + 1) % num_view_modes;
+    }
   }
   if (gb.buttons.pressed(BUTTON_UP)) {
-    cy = (cy + H - 1) % H;
+    if (paused) {
+      cy = (cy + H - 1) % H;
+    } else {
+      step_wait = std::max(step_wait - 1, 0);
+    }
   }
   if (gb.buttons.pressed(BUTTON_DOWN)) {
-    cy = (cy + 1) % H;
+    if (paused) {
+      cy = (cy + 1) % H;
+    } else {
+      step_wait = std::min(step_wait + 1, max_step_wait);
+    }
   }
   if (gb.buttons.pressed(BUTTON_A)) {
     LifeCa &ca = cas[0];
@@ -50,8 +71,10 @@ void testUpdate() {
   }
 
   if (!paused) {
-    for (auto& ca : cas) {
-      ca.step();
+    if (gb.frameCount % (1 << step_wait) == 0) {
+      for (auto& ca : cas) {
+        ca.step();
+      }
     }
   }
 }
@@ -59,21 +82,17 @@ void testUpdate() {
 void testDraw() {
   gb.display.clear();
 
-  if (paused) {
-    LifeCa &ca = cas[0];
-    for (int y = 0; y < H; ++y) {
-      for (int x = 0; x < W; ++x) {
-        if (ca.get(x, y)) {
-          gb.display.drawPixel(x, y, WHITE);
-        }
-      }
-    }
-    gb.display.drawPixel(cx, cy, YELLOW);
-  } else {
+  if (view_mode <= 4) {
     int layer = 0;
     for (const auto& ca : cas) {
-      ca.draw(layer++);
+      if (view_mode == 4 || layer == view_mode) {
+        ca.draw(layer);
+      }
+      ++layer;
     }
+  }
+  if (paused) {
+    gb.display.drawPixel(cx, cy, YELLOW);
   }
 
   displayCpuLoad();
