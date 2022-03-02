0548b5d7902905a97c76bce34647e0d9db6cc84e
Erwin Bonsma <eriban@users.sourceforge.net>
2022-03-02 22:24:20 +0100

    Decrease y-range of cell plot

--- generate_lookups.py
+++ generate_lookups.py
@@ -1,6 +1,6 @@
 # Generate look-up values used by CellCountHistory.cpp
 print(
     ", ".join("{:4d}".format(
-        max(0, 4 * i + i * (i + 1) // 2 - 2)
+        max(0, 4 * i + i * (i + 1) // 2 - 2) * 2 // 3
     ) for i in range(64))
 )
